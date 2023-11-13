7175ba73c2dbd37254cefaccec83ca9b78ade733
Alexandre Capt <acapt@adobe.com>
2023-11-13 16:29:39 +0100

    feat: integrating auth

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './pages/auth/auth.component';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://tcegujotdtoudscbpwqe.supabase.co',
+    supabaseKey: '',
+};
