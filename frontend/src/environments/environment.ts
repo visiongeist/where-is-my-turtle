59ec9365ba7dfc6063368072d2a8169d7189815d
Alexandre Capt <acapt@adobe.com>
2023-11-13 18:18:35 +0100

    fix: auth

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     supabaseUrl: 'https://tcegujotdtoudscbpwqe.supabase.co',
-    supabaseKey: '',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjZWd1am90ZHRvdWRzY2Jwd3FlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk4Njk4MzcsImV4cCI6MjAxNTQ0NTgzN30.ZodOezTsJhglISDGVhFNPNu4pBLzlswc0o4B2yPpRTQ',
 };
