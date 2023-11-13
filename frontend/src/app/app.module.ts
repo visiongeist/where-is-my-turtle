b4565ee63a3eef0ee136101aa6cfdf668a96f113
Alexandre Capt <acapt@adobe.com>
2023-11-13 17:01:47 +0100

    chore: add missing pieces

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './pages/auth/auth.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -20,6 +21,8 @@ import { MatDividerModule } from '@angular/material/divider';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     MatFormFieldModule,
     MatInputModule,
@@ -27,6 +30,7 @@ import { MatDividerModule } from '@angular/material/divider';
     MatButtonModule,
     MatButtonToggleModule,
     MatDividerModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
