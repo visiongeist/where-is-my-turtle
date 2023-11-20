63e7b32936d5973a08015f149514acf007059529
Damien Antipa <damien.antipa@gmail.com>
2023-11-20 09:41:28 +0100

    fix: deploy script to return output of image prediction

--- deploy-web.py
+++ deploy-web.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS,cross_origin
 from fastai.vision.all import *
-from PIL import Image
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
@@ -12,14 +11,13 @@ learn = load_learner('./model/model.pkl')
 categories = ('Dog', 'Cat')
 
 def classify_images(img):
-    print(img)
-    i = Image.open(img)
-    print(i)
+    i = PILImage.create(img)
     #'Is it a car?', 'Is it a car? but as zero or one', 'probabillity of [dog, cat]'
     pred, idx, probs = learn.predict(i)
     #return dictionary
     #zip together the categories and the
                                         #turn probs to float
+    
     return { 
         'prediction': categories[idx],
         'probabilities': dict(zip(categories, map(float, probs)))
@@ -28,7 +26,7 @@ def classify_images(img):
 # route for prediction
 @app.route('/predict', methods=['POST'])
 def predict():
-    jsonify(classify_images(request.files['image']))
+    return jsonify(classify_images(request.files['image']))
 
 if __name__ == '__main__':
     app.run()
